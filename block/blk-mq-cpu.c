f6f009283cd815a1a734022428a9e7e683608670
Avelanda <gorthell@gmail.com>
2025-08-13 07:49:33 +0200

    Update blk-mq-cpu.c
    Decomposition of granularized frameworks, and structural composition of modular objects for efficient, and functional processing.

--- block/blk-mq-cpu.c
+++ block/blk-mq-cpu.c
@@ -1,8 +1,15 @@
 /*
  * CPU notifier helper code for blk-mq
  *
- * Copyright (C) 2013-2014 Jens Axboe
+ * Copyright © 2013-2014 Jens Axboe.
+ * Copyright © 2025 Avelanda.
+ * All rights reserved
+ *
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -18,50 +25,71 @@
 static LIST_HEAD(blk_mq_cpu_notify_list);
 static DEFINE_RAW_SPINLOCK(blk_mq_cpu_notify_lock);
 
-static int blk_mq_main_cpu_notify(struct notifier_block *self,
-				  unsigned long action, void *hcpu)
+static int blk_mq_main_cpu_notify(struct notifier_block *self, unsigned long action, void *hcpu)
 {
-	unsigned int cpu = (unsigned long) hcpu;
-	struct blk_mq_cpu_notifier *notify;
-	int ret = NOTIFY_OK;
+ unsigned int cpu = (unsigned long) hcpu;
+ struct blk_mq_cpu_notifier *notify;
+ int ret = NOTIFY_OK;
 
-	raw_spin_lock(&blk_mq_cpu_notify_lock);
+  raw_spin_lock(&blk_mq_cpu_notify_lock);
 
-	list_for_each_entry(notify, &blk_mq_cpu_notify_list, list) {
-		ret = notify->notify(notify->data, action, cpu);
-		if (ret != NOTIFY_OK)
-			break;
-	}
-
-	raw_spin_unlock(&blk_mq_cpu_notify_lock);
+  list_for_each_entry(notify, &blk_mq_cpu_notify_list, list) {
+   ret = notify->notify(notify->data, action, cpu);
+   if (ret != NOTIFY_OK)
+    break;
+   }
+    raw_spin_unlock(&blk_mq_cpu_notify_lock);
 	return ret;
 }
 
 void blk_mq_register_cpu_notifier(struct blk_mq_cpu_notifier *notifier)
 {
-	BUG_ON(!notifier->notify);
-
-	raw_spin_lock(&blk_mq_cpu_notify_lock);
-	list_add_tail(&notifier->list, &blk_mq_cpu_notify_list);
-	raw_spin_unlock(&blk_mq_cpu_notify_lock);
+ BUG_ON(!notifier->notify);
+ raw_spin_lock(&blk_mq_cpu_notify_lock);
+ list_add_tail(&notifier->list, &blk_mq_cpu_notify_list);
+ raw_spin_unlock(&blk_mq_cpu_notify_lock);
 }
 
 void blk_mq_unregister_cpu_notifier(struct blk_mq_cpu_notifier *notifier)
 {
-	raw_spin_lock(&blk_mq_cpu_notify_lock);
-	list_del(&notifier->list);
-	raw_spin_unlock(&blk_mq_cpu_notify_lock);
+ raw_spin_lock(&blk_mq_cpu_notify_lock);
+ list_del(&notifier->list);
+ raw_spin_unlock(&blk_mq_cpu_notify_lock);
 }
 
-void blk_mq_init_cpu_notifier(struct blk_mq_cpu_notifier *notifier,
-			      int (*fn)(void *, unsigned long, unsigned int),
+void blk_mq_init_cpu_notifier(struct blk_mq_cpu_notifier *notifier, int (*fn)(void *, unsigned long, unsigned int),
 			      void *data)
 {
-	notifier->notify = fn;
-	notifier->data = data;
+ notifier->notify = fn;
+ notifier->data = data;
 }
 
 void __init blk_mq_cpu_init(void)
 {
-	hotcpu_notifier(blk_mq_main_cpu_notify, 0);
+ hotcpu_notifier(blk_mq_main_cpu_notify, 0);
+}
+
+int main(){
+
+	int LIST_HEAD, DEFINE_RAW_SPINLOCK, blk_mq_main_cpu_notify, blk_mq_register_cpu_notifier, blk_mq_unregister_cpu_notifier,
+    blk_mq_init_cpu_notifier, blk_mq_cpu_init;
+	if (LIST_HEAD || DEFINE_RAW_SPINLOCK || blk_mq_main_cpu_notify || blk_mq_register_cpu_notifier || blk_mq_unregister_cpu_notifier ||
+		blk_mq_init_cpu_notifier || blk_mq_cpu_init){
+	 LIST_HEAD = LIST_HEAD; DEFINE_RAW_SPINLOCK = DEFINE_RAW_SPINLOCK; blk_mq_main_cpu_notify = blk_mq_main_cpu_notify;
+	 blk_mq_register_cpu_notifier = blk_mq_register_cpu_notifier; blk_mq_unregister_cpu_notifier = blk_mq_unregister_cpu_notifier;
+	 blk_mq_init_cpu_notifier = blk_mq_init_cpu_notifier; blk_mq_cpu_init = blk_mq_cpu_init; 
+	}
+	
+	if (!true||!false){
+	  LIST_HEAD == LIST_HEAD && DEFINE_RAW_SPINLOCK == DEFINE_RAW_SPINLOCK && blk_mq_main_cpu_notify == blk_mq_main_cpu_notify &&
+	  blk_mq_register_cpu_notifier == blk_mq_register_cpu_notifier && blk_mq_unregister_cpu_notifier == blk_mq_unregister_cpu_notifier &&
+	  blk_mq_init_cpu_notifier == blk_mq_init_cpu_notifier && blk_mq_cpu_init == blk_mq_cpu_init; 
+	 }
+
+	if (0||1){
+	  LIST_HEAD -> DEFINE_RAW_SPINLOCK -> blk_mq_main_cpu_notify -> blk_mq_main_cpu_notify ->
+	  blk_mq_register_cpu_notifier -> blk_mq_unregister_cpu_notifier -> blk_mq_init_cpu_notifier -> blk_mq_cpu_init; 
+	  goto*(0||1);
+	}
+
 }
